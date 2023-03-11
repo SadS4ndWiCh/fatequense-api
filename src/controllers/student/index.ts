73dc8e4518970eaca7c4f049ceaa1c32645a1a06
sads4ndwich <caiocamargo007@gmail.com>
2023-03-11 20:11:04 -0300

    feat(siga): export all available controllers

---
+++ src/controllers/student/index.ts
@@ -0,0 +1,9 @@
+import { partialGradeController } from "./partialGrade.controller";
+import { profileController } from "./profile.controller";
+import { scheduleController } from "./schedule.controller";
+
+export {
+	profileController,
+	scheduleController,
+	partialGradeController,
+}
