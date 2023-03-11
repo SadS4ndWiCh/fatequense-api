25d6fc1807ff3a79ca71842e16ae4f8cda850363
sads4ndwich <caiocamargo007@gmail.com>
2023-03-11 20:06:58 -0300

    refactor(siga): change to schema parse instead safeParse

--- src/libs/siga/scrappers/student/profile.scrapper.ts
+++ src/libs/siga/scrappers/student/profile.scrapper.ts
@@ -1,30 +1,10 @@
-import { z } from 'zod';
-
 import type { ExtractedGXState } from "../utils/gxstate.utils";
 import { toTitleCase } from "../utils/to-title-case.utils";
 
-export const studentProfileSchema = z.object({
-	name: z.string().min(1),
-	personalEmail: z.string().email(),
-	institutionalEmail: z.string().email(),
-	cpf: z.string(),
-	birthday: z.string(),
-	averageGrade: z.number(),
-	progression: z.number(),
-	photoUrl: z.string().url(),
-	college: z.object({
-		name: z.string(),
-		courseName: z.string(),
-		currentSemester: z.number(),
-		coursePeriod: z.string(),
-		state: z.string()
-	})
-});
-
-export type Profile = z.TypeOf<typeof studentProfileSchema>;
+import { studentProfileSchema } from "./schemas/profile.schema";
 
 export function getStudentProfile({ $, ...gxstate }: ExtractedGXState) {
-	return studentProfileSchema.safeParse({
+	return studentProfileSchema.parse({
 		name: toTitleCase(gxstate.get('vPRO_PESSOALNOME').replace(' -', '')),
 		personalEmail: gxstate.get('vPRO_PESSOALEMAIL'),
 		institutionalEmail: gxstate.get('vINSTITUCIONALFATEC', true),
