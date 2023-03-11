ae9fb9a1c51d780f0db8f5b2c9f7393e681551d2
sads4ndwich <caiocamargo007@gmail.com>
2023-03-11 20:05:29 -0300

    feat(siga): added student schedule schema

---
+++ src/libs/siga/scrappers/student/schemas/schedule.schema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const studentScheduleSchema = z.array(
+	z.array(
+		z.object({
+			cod: z.string().length(6),
+			discipline: z.object({
+				name: z.string().min(1),
+				teacherName: z.string().min(1),
+			}).optional(),
+			startsAt: z.string().datetime(),
+			endsAt: z.string().datetime(),
+		})
+	)
+);
+
+export type Schedule = z.infer<typeof studentScheduleSchema>;
