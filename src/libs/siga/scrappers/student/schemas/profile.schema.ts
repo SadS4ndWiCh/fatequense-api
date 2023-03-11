7bf303f5fd2580181e999784f437e78b182ce002
sads4ndwich <caiocamargo007@gmail.com>
2023-03-11 20:05:10 -0300

    feat(siga): added student profile schema

---
+++ src/libs/siga/scrappers/student/schemas/profile.schema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const studentProfileSchema = z.object({
+	name: z.string().min(1),
+	personalEmail: z.string().email(),
+	institutionalEmail: z.string().email(),
+	cpf: z.string(),
+	birthday: z.string(),
+	averageGrade: z.number(),
+	progression: z.number(),
+	photoUrl: z.string().url(),
+	college: z.object({
+		name: z.string(),
+		courseName: z.string(),
+		currentSemester: z.number(),
+		coursePeriod: z.string(),
+		state: z.string()
+	})
+});
+
+export type Profile = z.infer<typeof studentProfileSchema>;
+
