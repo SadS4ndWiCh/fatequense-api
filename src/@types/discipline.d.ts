47bd0786e8c84ff8ecd55dca34739f991391e6da
sads4ndwich <caiocamargo007@gmail.com>
2023-03-13 17:21:04 -0300

    type: added discipline types

---
+++ src/@types/discipline.d.ts
@@ -0,0 +1,72 @@
+interface IDiscipline {
+	name: string;
+	teacherName: string;
+}
+
+interface IDisciplineExamDate {
+	title: ExamTitle;
+	startsAt: string;
+	grade: number;
+}
+
+interface IDisciplinePartialGrade {
+	cod: string;
+	disciplineName: string;
+	averageGrade: number;
+	examsDates: IDisciplineExamDate[];
+}
+
+interface IDisciplineLessons {
+	title: string;
+	date: string;
+	presences: number;
+	absences: number;
+}
+
+interface IDisciplinePartialAbsences {
+	cod: string;
+	disciplineName: string;
+	totalPresences: number;
+	totalAbsences: number;
+	lessons: IDisciplineLessons[];
+}
+
+interface ISchedule {
+	cod: string;
+	startsAt: Date;
+	endsAt: Date;
+	discipline: IDiscipline | null;
+}
+
+type DisciplineState =
+	| 'dismissed'
+	| 'approved'
+	| 'attending'
+	| 'not-attended'
+	| 'unknown';
+
+interface IDisciplineGrade {
+	cod: string;
+	classHours: number;
+	name: string;
+	state: DisciplineState;
+	period?: string;
+	frequency?: number;
+	grade?: number;
+}
+
+interface ISemester {
+	number: number;
+	disciplines: IDisciplineGrade[];
+}
+
+interface IDisciplineHistory {
+	cod: string;
+	disciplineName: string;
+	description: string;
+	finalGrade: number;
+	totalAbscences: number;
+	presenceFrequency: number;
+	renunciationAt: string;
+	isApproved: boolean;
+}
