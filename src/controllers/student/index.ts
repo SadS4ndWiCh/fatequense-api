081233071f84954f3946754091a78471ff7051af
sads4ndwich <caiocamargo007@gmail.com>
2023-03-13 17:22:09 -0300

    feat(controller): added controllers

--- src/controllers/student/index.ts
+++ src/controllers/student/index.ts
@@ -1,9 +1,13 @@
+import { historyController } from "./history.controller";
+import { partialAbsencesController } from "./partialAbsences.controller";
 import { partialGradeController } from "./partialGrade.controller";
 import { profileController } from "./profile.controller";
 import { scheduleController } from "./schedule.controller";
 
 export {
 	profileController,
+	historyController,
 	scheduleController,
 	partialGradeController,
+	partialAbsencesController,
 }
