404f81d4c46e9f2d6fe6d929c390f9e445470f2a
sads4ndwich <caiocamargo007@gmail.com>
2023-03-14 18:32:22 -0300

    feat: initialize app

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
-import fastify from "fastify";
 import './libs/dayjs.config';
+
+import fastify from "fastify";
 
 import { studentRoutes } from "./routes/student.routes";
 
 const app = fastify();
 
-app.register(studentRoutes, { prefix: '/student' });
-
-app.listen({
-	host: '0.0.0.0',
-	port: process.env.PORT ? Number(process.env.PORT) : 3333,
-}).then(() => {
-	console.log("🚀 Server is running...")
-});
+app
+	.register(studentRoutes, { prefix: '/student' })
+	.listen({
+		host: '0.0.0.0',
+		port: process.env.PORT ? Number(process.env.PORT) : 3333,
+	}).then(() => {
+		console.log("🚀 Server is running...")
+	});
