0c272e5a02adeec07d8d780258985dc13c36f6dc
sads4ndwich <caiocamargo007@gmail.com>
2023-03-14 18:31:25 -0300

    feat(route): define routes and hooks to student section

--- src/routes/student.routes.ts
+++ src/routes/student.routes.ts
@@ -1,47 +1,24 @@
 import { FastifyInstance } from "fastify";
 
-import { get } from "~/libs/siga/siga.api";
+import {
+	historyController,
+	partialAbsencesController,
+	partialGradeController,
+	profileController,
+	scheduleController
+} from "~/controllers/student";
+import { useCache } from "~/hooks/cache.hook";
 import { isAuthenticated } from "~/libs/siga/siga.validations";
-import { getAuthorizationToken } from "~/libs/siga/siga.utils";
-import { getStudentProfile } from "~/libs/siga/scrappers/student/profile.scrapper";
-import { extractGXStateOfHTML } from "~/libs/siga/scrappers/utils/gxstate.utils";
-
-import { AUTH_COOKIE_FIELD_NAME } from "~/libs/siga/siga.consts";
-import { getSchedule } from "~/libs/siga/scrappers/student/schedule.scrapper";
 
 export async function studentRoutes(app: FastifyInstance) {
-	app.get('/profile', { preValidation: isAuthenticated }, async (req, reply) => {
-		const token = getAuthorizationToken(req.headers);
+	const cache = useCache();
 
-		const { data: html, success } = await get({
-			route: 'home',
-			headers: {
-				cookie: `${AUTH_COOKIE_FIELD_NAME}=${token}`
-			}
-		});
+	app.addHook('onRequest', cache.onRequest);
+	app.addHook('onSend', cache.onSend);
 
-		if (!success) return reply.status(500).send({ error: 'Failed to fetch page' });
-
-		const profile = getStudentProfile(extractGXStateOfHTML(html));
-		if (!profile.success) return reply.status(500).send({ error: profile.error });
-
-		return { profile: profile.data };
-	});
-
-	app.get('/schedule', { preValidation: isAuthenticated }, async (req, reply) => {
-		const token = getAuthorizationToken(req.headers);
-
-		const { data: html, success } = await get({
-			route: 'schedule',
-			headers: {
-				cookie: `${AUTH_COOKIE_FIELD_NAME}=${token}`
-			}
-		});
-
-		if (!success) return reply.status(500).send({ error: 'Failed to fetch page' });
-		
-		const schedule = getSchedule(extractGXStateOfHTML(html));
-	
-		return { schedule };
-	});
+	app.get('/profile', { preValidation: isAuthenticated }, profileController);
+	app.get('/history', { preValidation: isAuthenticated }, historyController);
+	app.get('/schedule', { preValidation: isAuthenticated }, scheduleController);
+	app.get('/partialGrade', { preValidation: isAuthenticated }, partialGradeController);
+	app.get('/partialAbsences', { preValidation: isAuthenticated }, partialAbsencesController);
 }
