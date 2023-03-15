37989b8a6a7edfd18907272492002be8ec91719f
sads4ndwich <caiocamargo007@gmail.com>
2023-03-15 17:28:57 -0300

    feat(env): use zod to type-safe enviroment variables

---
+++ src/utils/env.utils.ts
@@ -0,0 +1,14 @@
+import * as dotenv from 'dotenv';
+dotenv.config({ path: __dirname + '/../../.env.local' });
+
+import { z } from "zod";
+
+const envSchema = z.object({
+	PORT: z.string().transform(port => Number(port)),
+
+	JWT_SECRET_KEY: z.string().min(1),
+	JWT_EXPIRES_IN: z.string().min(1),
+	JWT_ALGORITHM: z.string().min(1),
+});
+
+export const env = envSchema.parse(process.env);
