9526a9971536d102bec26445f59d3869e6f28752
sads4ndwich <caiocamargo007@gmail.com>
2023-03-15 17:34:35 -0300

    feat(route): register auth routes

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import './libs/dayjs.config';
 import fastify from "fastify";
 
 import { studentRoutes } from "./routes/student.routes";
+import { authRoutes } from './routes/auth.routes';
 
 const app = fastify();
 
 app
 	.register(studentRoutes, { prefix: '/student' })
+	.register(authRoutes, { prefix: '/auth' })
 	.listen({
 		host: '0.0.0.0',
 		port: process.env.PORT ? Number(process.env.PORT) : 3333,
