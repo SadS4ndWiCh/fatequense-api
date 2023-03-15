e1a22cb5e32bbba44f140a2ff7eb9580ac5e2525
sads4ndwich <caiocamargo007@gmail.com>
2023-03-15 17:29:39 -0300

    feat(jwt): abstract sign and verify jwt functions

---
+++ src/utils/jwt.utils.ts
@@ -0,0 +1,36 @@
+import { sign as jwtSign, verify as jwtVerify } from "jsonwebtoken";
+import type { VerifyOptions, Algorithm, SignOptions } from "jsonwebtoken";
+
+import { env } from "./env.utils";
+
+type SignProps = {
+	payload: string | object | Buffer,
+	options?: SignOptions
+}
+
+type VerifyProps = Omit<SignProps, 'payload'> & {
+	token: string;
+};
+
+export function sign({ payload, options }: SignProps) {
+	const jwtOptions: SignOptions = {
+		expiresIn: env.JWT_EXPIRES_IN,
+		algorithm: env.JWT_ALGORITHM as Algorithm,
+		...options
+	};
+
+	return jwtSign(payload, env.JWT_SECRET_KEY, jwtOptions);
+}
+
+export function verify({ token, options }: VerifyProps) {
+	const jwtOptions: VerifyOptions = {
+		algorithm: [env.JWT_ALGORITHM as Algorithm],
+		...options
+	};
+
+	try {
+		return jwtVerify(token, env.JWT_SECRET_KEY, jwtOptions);
+	} catch {
+		return null;
+	}
+}
