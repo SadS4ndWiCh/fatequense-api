f6b8fe43a8daf1513aa3a6a290ec715288eac411
sads4ndwich <caiocamargo007@gmail.com>
2023-03-15 17:30:06 -0300

    feat: parse cookies string

---
+++ src/utils/parse-cookie.utils.ts
@@ -0,0 +1,11 @@
+export function parseCookie(cookies: string) {
+	const allCookies: Record<string, string | boolean> = {};
+
+	cookies.split(';').forEach(cookie => {
+		const [cookieName, cookieValue] = cookie.split('=');
+
+		allCookies[cookieName.trim()] = cookieValue ? cookieValue.trim() : true;
+	});
+
+	return allCookies;
+}
