267cba4e0aa423c0b78e02e54da027d6c81ce81c
sads4ndwich <caiocamargo007@gmail.com>
2023-03-16 21:25:03 -0300

    test(utils): added test to validate toTitleCase function

---
+++ src/libs/siga/scrappers/utils/to-title-case.utils.spec.ts
@@ -0,0 +1,9 @@
+import { toTitleCase } from "./to-title-case.utils";
+
+describe('toTitleCase util function', () => {
+	it('should be able to title case a text', () => {
+		expect(toTitleCase('hello world')).toBe('Hello World');
+		expect(toTitleCase('HELLO WORLD')).toBe('Hello World');
+		expect(toTitleCase('Hello World')).toBe('Hello World');
+	});
+});
