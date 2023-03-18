7a0ceb51f18f45fee954122d02d359675c0df6c0
sads4ndwich <caiocamargo007@gmail.com>
2023-03-17 21:26:33 -0300

    test(utils): included partialGrade, partialAbsences, schedule and history pages

--- src/test/utils/get-page-html.utils.ts
+++ src/test/utils/get-page-html.utils.ts
@@ -2,7 +2,11 @@ import fs from "fs";
 import path from "path";
 
 const HTML_PAGES_PATH = {
-	home: path.join(__dirname, '..', 'sources', 'pages', 'home.html')
+	home: path.join(__dirname, '..', 'sources', 'pages', 'home.html'),
+	partialGrade: path.join(__dirname, '..', 'sources', 'pages', 'partialGrade.html'),
+	partialAbsences: path.join(__dirname, '..', 'sources', 'pages', 'partialAbsences.html'),
+	schedule: path.join(__dirname, '..', 'sources', 'pages', 'schedule.html'),
+	history: path.join(__dirname, '..', 'sources', 'pages', 'history.html'),
 };
 
 export function getPageHtml(page: keyof typeof HTML_PAGES_PATH) {
