4d2fad99628c3e1df99b08e3ab31ed72513ba58a
sads4ndwich <caiocamargo007@gmail.com>
2023-03-17 21:27:21 -0300

    test(scrapper): added test to remaining scrapping function

---
+++ src/libs/siga/scrappers/student/partialGrade.scrapper.spec.ts
@@ -0,0 +1,18 @@
+import { getPageHtml } from "~/test/utils/get-page-html.utils";
+import { extractGXStateOfHTML } from "../utils/gxstate.utils";
+import { getPartialGrade } from "./partialGrade.scrapper";
+
+describe('Partial Grade scrapper function', () => {
+	it('should be able to get student partial grade from HTML', () => {
+		const partialGradeHtmlContent = getPageHtml('partialGrade');
+
+		const partialGrade = getPartialGrade(extractGXStateOfHTML(partialGradeHtmlContent));
+
+		expect(partialGrade.length).toBeGreaterThanOrEqual(1);
+		expect(partialGrade).toContainEqual(
+			expect.objectContaining({
+				cod: expect.any(String),
+			})
+		)
+	});
+});
