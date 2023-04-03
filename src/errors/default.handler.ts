83e7f4759629ea8c4357d4495cbae680e77d1658
sads4ndwich <caiocamargo007@gmail.com>
2023-04-02 22:39:20 -0300

    feat(error): added a default error handler

---
+++ src/errors/default.handler.ts
@@ -0,0 +1,12 @@
+import type { FastifyError, FastifyReply, FastifyRequest } from "fastify";
+import { SigaError } from "~/libs/siga/errors/SigaError.error";
+
+export function defaultErrorHandler(err: FastifyError, req: FastifyRequest, reply: FastifyReply) {
+	if (err instanceof SigaError) return err.throwStatusError(reply);
+	else if (err?.statusCode === 429) return reply.status(429).send(err);
+
+	return reply.status(500).send({
+		statusCode: 500,
+		error: 'Internal Server Error'
+	})
+}
