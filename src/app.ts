5fc096bde0b7bbb9ec30f021ab04f799b6510596
sads4ndwich <caiocamargo007@gmail.com>
2023-04-02 22:40:22 -0300

    feat(error): use default error handler as global

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,10 @@ import cors from '@fastify/cors';
 
 import { studentRoutes } from "./routes/student.routes";
 import { authRoutes } from './routes/auth.routes';
+import { defaultErrorHandler } from './errors/default.handler';
 
 export const app = fastify()
 	.register(cors, { origin: true })
 	.register(studentRoutes, { prefix: '/student' })
 	.register(authRoutes, { prefix: '/auth' })
+	.setErrorHandler(defaultErrorHandler)
