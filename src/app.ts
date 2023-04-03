b0e3fed72a659b305227a9066af0347c9ee214f6
sads4ndwich <caiocamargo007@gmail.com>
2023-04-02 22:24:58 -0300

    feat(cors): enable cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import './libs/dayjs.config';
 
 import fastify from "fastify";
+import cors from '@fastify/cors';
 
 import { studentRoutes } from "./routes/student.routes";
 import { authRoutes } from './routes/auth.routes';
 
 export const app = fastify()
+	.register(cors, { origin: true })
 	.register(studentRoutes, { prefix: '/student' })
 	.register(authRoutes, { prefix: '/auth' })
