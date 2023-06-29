43344c6e61a738abf1611c69967dd601f8d1af74
sads4ndwich <caiocamargo007@gmail.com>
2023-06-29 14:19:33 -0300

    feat(controller): added edit profile controller

---
+++ src/controllers/student/edit-profile.controller.ts
@@ -0,0 +1,25 @@
+import type { FastifyRequest } from 'fastify';
+
+import { db } from '~/libs/db';
+import { studentDatabaseSchema } from '~/libs/validations/db';
+
+import { profileController } from './profile.controller';
+
+export async function editProfileController(req: FastifyRequest) {
+  const dataToUpdate = studentDatabaseSchema.parse(
+    JSON.parse(req.body as string),
+  );
+
+  const { profile } = await profileController(req);
+
+  await db.student.upsert({
+    where: { id: profile.ra },
+    update: { ...dataToUpdate },
+    create: {
+      id: profile.ra,
+      ...dataToUpdate,
+    },
+  });
+
+  return { ok: true };
+}
