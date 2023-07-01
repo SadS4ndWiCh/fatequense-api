da7745d3d98fbb8b652156e33069d586a0ea49df
sads4ndwich <caiocamargo007@gmail.com>
2023-07-01 18:24:13 -0300

    feat(controller): edit student profile or create if does not exists

--- src/controllers/student/edit-profile.controller.ts
+++ src/controllers/student/edit-profile.controller.ts
@@ -1,25 +1,29 @@
+import { eq } from 'drizzle-orm';
 import type { FastifyRequest } from 'fastify';
+import { db } from '~/db';
+import { students } from '~/db/schema';
 
-import { db } from '~/libs/db';
 import { studentDatabaseSchema } from '~/libs/validations/db';
 
 import { profileController } from './profile.controller';
 
 export async function editProfileController(req: FastifyRequest) {
-  const dataToUpdate = studentDatabaseSchema.parse(
-    JSON.parse(req.body as string),
-  );
+  const dataToUpdate = studentDatabaseSchema.parse(req.body);
 
   const profile = await profileController(req);
 
-  await db.student.upsert({
-    where: { id: profile.ra },
-    update: { ...dataToUpdate },
-    create: {
-      id: profile.ra,
-      ...dataToUpdate,
-    },
-  });
+  const isStudentAlreadyRegistered =
+    (await db.select().from(students).where(eq(students.id, profile.ra)))
+      .length === 1;
+
+  if (!isStudentAlreadyRegistered) {
+    await db.insert(students).values({ id: profile.ra, ...dataToUpdate });
+  } else {
+    await db
+      .update(students)
+      .set(dataToUpdate)
+      .where(eq(students.id, profile.ra));
+  }
 
   return { ok: true };
 }
