febbfc269777437974430d1976213018e90983a0
sads4ndwich <caiocamargo007@gmail.com>
2023-07-01 18:25:11 -0300

    feat(controller): retrieve student with custom photoUrl if it has edited

--- src/controllers/student/profile.controller.ts
+++ src/controllers/student/profile.controller.ts
@@ -1,4 +1,7 @@
+import { eq } from 'drizzle-orm';
 import type { FastifyRequest } from 'fastify';
+import { db } from '~/db';
+import { students } from '~/db/schema';
 
 import { getStudentProfile } from '~/core/scrapers/siga/handlers/profile.scraper';
 import { get } from '~/core/scrapers/siga/siga.network';
@@ -10,5 +13,19 @@ export async function profileController(req: FastifyRequest) {
 
   const { data: html } = await get({ route: '/aluno/home.aspx', token });
 
-  return getStudentProfile(html);
+  const profile = getStudentProfile(html);
+
+  const student = await db
+    .select()
+    .from(students)
+    .where(eq(students.id, profile.ra));
+
+  if (student.length === 1) {
+    return {
+      ...profile,
+      photoUrl: student[0].photoUrl ? student[0].photoUrl : profile.photoUrl,
+    };
+  }
+
+  return profile;
 }
