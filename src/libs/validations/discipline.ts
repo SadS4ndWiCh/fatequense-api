642aac14013d5e12c6776ee50991d17a80b4bdf1
sads4ndwich <caiocamargo007@gmail.com>
2023-07-01 14:00:55 -0300

    refactor: move validations

---
+++ src/libs/validations/discipline.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+
+export const studentDisciplineSchema = z.object({
+  name: z.string().min(1),
+  code: z.string().min(1),
+  class: z.string().min(1),
+  teacherName: z
+    .string()
+    .transform((teacherName) =>
+      teacherName
+        .toLowerCase()
+        .replace(/(^|\s)\S/g, (char) => char.toUpperCase()),
+    ),
+  syllabus: z.string().min(1),
+  goal: z.string().min(1),
+  workload: z.object({
+    weekly: z.coerce.number(),
+    theorical: z.coerce.number(),
+    practical: z.coerce.number(),
+    total: z.coerce.number(),
+  }),
+  totalAbsencesAllowed: z.coerce.number(),
+});
---
+++ src/libs/validations/history.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const studentHistorySchema = z.array(
+  z.object({
+    cod: z.string().length(6),
+    disciplineName: z.string().min(1),
+    description: z.string(),
+    finalGrade: z.number(),
+    presenceFrequency: z.number(),
+    renunciationAt: z.string().nullable(),
+    isApproved: z.boolean(),
+  }),
+);
