e8606e5edbacbf80aaafffe25706213c6b73d77f
sads4ndwich <caiocamargo007@gmail.com>
2023-07-06 15:05:04 -0300

    feat(controller): export school grade controller

--- src/controllers/student/index.ts
+++ src/controllers/student/index.ts
@@ -1,7 +1,9 @@
+export * as disciplines from './disciplines';
+export { editProfileController } from './edit-profile.controller';
 export { historyController } from './history.controller';
 export { partialAbsencesController } from './partial-absences.controller';
 export { partialGradeController } from './partial-grade.controller';
 export { profileController } from './profile.controller';
-export { editProfileController } from './edit-profile.controller';
 export { scheduleController } from './schedule.controller';
-export * as disciplines from './disciplines';
+export { schoolGradeController } from './school-grade.controller';
+
