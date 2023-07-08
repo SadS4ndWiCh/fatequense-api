f3a4b514661a6284fe9093c7badb5e38f22ebc39
sads4ndwich <caiocamargo007@gmail.com>
2023-07-08 18:15:05 -0300

    feat(error): use `err.issues` if error is instance of `ZodError`

--- src/errors/default.handler.ts
+++ src/errors/default.handler.ts
@@ -1,4 +1,5 @@
 import type { FastifyError, FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
 
 import { SigaError } from '~/core/scrapers/siga/errors/siga-error.error';
 
@@ -9,6 +10,7 @@ export function defaultErrorHandler(
 ) {
   if (err instanceof SigaError) return err.throwStatusError(reply);
   else if (err?.statusCode === 429) return reply.status(429).send(err);
+  else if (err instanceof z.ZodError) return reply.status(500).send(err.issues);
 
   return reply.status(500).send({
     statusCode: 500,
