46604477bee7eb2dc114749b7eece29ad9e418bb
sads4ndwich <caiocamargo007@gmail.com>
2023-07-29 14:27:46 -0300

    feat: use new discipline details schema and type

--- src/core/scrapers/siga/handlers/discipline.scraper.ts
+++ src/core/scrapers/siga/handlers/discipline.scraper.ts
@@ -1,6 +1,6 @@
-import { StudentDiscipline } from '~/types';
+import { StudentDisciplineDetails } from '~/types';
 
-import { studentDisciplineSchema } from '~/libs/validations/discipline';
+import { studentDisciplineDetailsSchema } from '~/libs/validations/discipline';
 
 import { DisciplineNotFound } from '../errors/discipline-not-found.error';
 
@@ -8,29 +8,31 @@ import { withGXState } from '../utils/gxstate.utils';
 
 import { MINIMUM_ATTENDANCE_PERCENTAGE } from '../siga.constants';
 
-export const getStudentDiscipline = withGXState<StudentDiscipline>(({ $ }) => {
-  const isDisciplineInvalid = !!$('#gxErrorViewer').text();
-  if (isDisciplineInvalid) throw new DisciplineNotFound();
+export const getStudentDiscipline = withGXState<StudentDisciplineDetails>(
+  ({ $ }) => {
+    const isDisciplineInvalid = !!$('#gxErrorViewer').text();
+    if (isDisciplineInvalid) throw new DisciplineNotFound();
 
-  const totalWorkload = Number(
-    $('#span_W0008W0013vACD_DISCIPLINAAULASTOTAISPERIODO').text(),
-  );
-  const totalAbsencesAllowed =
-    totalWorkload - totalWorkload * MINIMUM_ATTENDANCE_PERCENTAGE;
+    const totalWorkload = Number(
+      $('#span_W0008W0013vACD_DISCIPLINAAULASTOTAISPERIODO').text(),
+    );
+    const totalAbsencesAllowed =
+      totalWorkload - totalWorkload * MINIMUM_ATTENDANCE_PERCENTAGE;
 
-  return studentDisciplineSchema.parse({
-    name: $('#span_W0005vACD_DISCIPLINANOME').text(),
-    code: $('#span_W0005vSHOW_ACD_DISCIPLINASIGLA').text(),
-    class: $('#span_W0005vACD_TURMALETRA').text(),
-    teacherName: $('#span_W0005vPRO_PESSOALNOME').text(),
-    syllabus: $('#span_W0008W0013vACD_DISCIPLINAEMENTA').text(),
-    goal: $('#span_W0008W0013vACD_DISCIPLINAOBJETIVO').text(),
-    workload: {
-      weekly: $('#span_W0008W0013vACD_DISCIPLINAAULASSEMANAIS').text(),
-      theorical: $('#span_W0008W0013vACD_DISCIPLINAAULASTEORICAS').text(),
-      practical: $('#span_W0008W0013vACD_DISCIPLINAAULASPRATICAS').text(),
-      total: totalWorkload,
-    },
-    totalAbsencesAllowed,
-  });
-});
+    return studentDisciplineDetailsSchema.parse({
+      name: $('#span_W0005vACD_DISCIPLINANOME').text(),
+      code: $('#span_W0005vSHOW_ACD_DISCIPLINASIGLA').text(),
+      class: $('#span_W0005vACD_TURMALETRA').text(),
+      teacherName: $('#span_W0005vPRO_PESSOALNOME').text(),
+      syllabus: $('#span_W0008W0013vACD_DISCIPLINAEMENTA').text(),
+      goal: $('#span_W0008W0013vACD_DISCIPLINAOBJETIVO').text(),
+      workload: {
+        weekly: $('#span_W0008W0013vACD_DISCIPLINAAULASSEMANAIS').text(),
+        theorical: $('#span_W0008W0013vACD_DISCIPLINAAULASTEORICAS').text(),
+        practical: $('#span_W0008W0013vACD_DISCIPLINAAULASPRATICAS').text(),
+        total: totalWorkload,
+      },
+      totalAbsencesAllowed,
+    });
+  },
+);
