0870f6af42e83101e35d121a400498510a666d40
sads4ndwich <caiocamargo007@gmail.com>
2023-07-31 11:20:11 -0300

    feat: added basic security with helmet

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import './libs/dayjs.config';
 
 import cors from '@fastify/cors';
+import helmet from '@fastify/helmet';
 import fastify from 'fastify';
 
 import { routes } from './routes/index.routes';
@@ -11,6 +12,7 @@ import { env } from './utils/env.utils';
 
 export const app = fastify()
   .register(cors, { origin: true })
+  .register(helmet, { global: true })
   .register(import('@fastify/redis'), { url: env.UPSTASH_REDIS_REST_URL })
   .register(routes, { prefix: '/' })
   .setErrorHandler(defaultErrorHandler);
