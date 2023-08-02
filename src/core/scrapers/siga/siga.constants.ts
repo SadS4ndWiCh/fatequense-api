a66f523eeda52c02e514964b36c593103f18cb32
sads4ndwich <caiocamargo007@gmail.com>
2023-08-02 10:37:51 -0300

    feat: added list with random user agents

--- src/core/scrapers/siga/siga.constants.ts
+++ src/core/scrapers/siga/siga.constants.ts
@@ -6,9 +6,38 @@ export const USER_INPUT_ID = 'vSIS_USUARIOID';
 export const PASS_INPUT_ID = 'vSIS_USUARIOSENHA';
 export const BTN_CONFIRM_ID = 'BTCONFIRMA';
 
-export const USER_AGENT =
-  'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 ' +
-  '(KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36';
+export const USER_AGENTS = [
+  'Mozilla/5.0 (Windows; U; Windows NT 6.3;) AppleWebKit/600.7 (KHTML, like Gecko) Chrome/47.0.3418.105 Safari/602',
+  'Mozilla/5.0 (Linux; Linux i653 x86_64; en-US) Gecko/20100101 Firefox/46.5',
+  'Mozilla/5.0 (Linux; U; Linux i586 x86_64) Gecko/20100101 Firefox/55.4',
+  'Mozilla/5.0 (Linux; U; Android 4.4; IQ4500 Quad Build/KOT49H) AppleWebKit/603.43 (KHTML, like Gecko)  Chrome/55.0.2865.222 Mobile Safari/534.8',
+  'Mozilla/5.0 (Linux; Linux i666 x86_64) AppleWebKit/534.4 (KHTML, like Gecko) Chrome/47.0.2528.207 Safari/602',
+  'Mozilla/5.0 (Linux; U; Android 5.1.1; SAMSUNG SM-G925M Build/LMY47X) AppleWebKit/601.44 (KHTML, like Gecko)  Chrome/55.0.1617.183 Mobile Safari/537.9',
+  'Mozilla/5.0 (Android; Android 4.4; LG-E989 Build/KOT49I) AppleWebKit/601.10 (KHTML, like Gecko)  Chrome/55.0.2447.194 Mobile Safari/536.3',
+  'Mozilla/5.0 (Linux; U; Linux x86_64) Gecko/20130401 Firefox/74.4',
+  'Mozilla/5.0 (Linux x86_64; en-US) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/49.0.3865.371 Safari/601',
+  'Mozilla/5.0 (Linux i545 x86_64) Gecko/20130401 Firefox/69.2',
+  'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_7; en-US) AppleWebKit/602.20 (KHTML, like Gecko) Chrome/54.0.2420.391 Safari/536',
+  'Mozilla/5.0 (Windows; Windows NT 10.3;; en-US) AppleWebKit/537.12 (KHTML, like Gecko) Chrome/49.0.3761.105 Safari/603',
+  'Mozilla/5.0 (iPod; CPU iPod OS 8_7_5; like Mac OS X) AppleWebKit/536.7 (KHTML, like Gecko)  Chrome/54.0.3347.307 Mobile Safari/600.6',
+  'Mozilla/5.0 (Windows; Windows NT 10.3; WOW64) AppleWebKit/602.25 (KHTML, like Gecko) Chrome/54.0.3489.375 Safari/600.0 Edge/15.27648',
+  'Mozilla/5.0 (Linux i570 x86_64) Gecko/20130401 Firefox/47.3',
+  'Mozilla/5.0 (Linux; U; Android 5.0.2; SAMSUNG-SM-N915A Build/LRX22C) AppleWebKit/537.12 (KHTML, like Gecko)  Chrome/54.0.3286.361 Mobile Safari/536.6',
+  'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0_2; like Mac OS X) AppleWebKit/537.3 (KHTML, like Gecko)  Chrome/54.0.2253.332 Mobile Safari/602.7',
+  'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 7P Build/MDB08L) AppleWebKit/601.49 (KHTML, like Gecko)  Chrome/54.0.1263.308 Mobile Safari/535.4',
+  'Mozilla/5.0 (Linux; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/53.0.3196.111 Safari/603',
+  'Mozilla/5.0 (compatible; MSIE 11.0; Windows NT 6.3; Win64; x64; en-US Trident/7.0)',
+  'Mozilla/5.0 (U; Linux i685 x86_64; en-US) AppleWebKit/600.39 (KHTML, like Gecko) Chrome/53.0.2876.363 Safari/603',
+  'Mozilla/5.0 (Android; Android 5.1.1; MOTOROLA MOTO X PURE XT1575 Build/LPC23) AppleWebKit/600.48 (KHTML, like Gecko)  Chrome/48.0.1350.339 Mobile Safari/601.7',
+  'Mozilla/5.0 (compatible; MSIE 11.0; Windows; Windows NT 6.3; WOW64 Trident/7.0)',
+  'Mozilla/5.0 (compatible; MSIE 7.0; Windows; Windows NT 10.5; Win64; x64 Trident/4.0)',
+  'Mozilla/5.0 (compatible; MSIE 7.0; Windows; Windows NT 6.0; Trident/4.0)',
+  'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 7_8_4; en-US) AppleWebKit/600.48 (KHTML, like Gecko) Chrome/54.0.2530.128 Safari/602',
+  'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 7_0_9; en-US) AppleWebKit/600.24 (KHTML, like Gecko) Chrome/49.0.3180.300 Safari/602',
+  'Mozilla/5.0 (Linux; U; Android 5.1.1; SM-G9208 Build/LMY47X) AppleWebKit/603.5 (KHTML, like Gecko)  Chrome/47.0.1224.199 Mobile Safari/603.3',
+  'Mozilla/5.0 (compatible; MSIE 9.0; Windows; Windows NT 10.5; Win64; x64 Trident/5.0)',
+  'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 9_8_6) Gecko/20130401 Firefox/72.7',
+];
 
 export const GX_STATE = `{"_EventName":"E'EVT_CONFIRMAR'.","_EventGridId":"","_EventRowId":"","MPW0005_CMPPGM":"login_top.aspx","MPW0005GX_FocusControl":"","vSAIDA":"","vREC_SIS_USUARIOID":"","GX_FocusControl":"vSIS_USUARIOID","GX_AJAX_KEY":"D1ABEAAE18039C602CD8FD7385F248CD","AJAX_SECURITY_TOKEN":"84F199CDDB0C58592A768EFD8B54412708BB9745AE6F886B5BA6C30E7C998AEA","GX_CMP_OBJS":{"MPW0005":"login_top"},"sCallerURL":"","GX_RES_PROVIDER":"GXResourceProvider.aspx","GX_THEME":"GeneXusX","_MODE":"","Mode":"","IsModified":"1"}`;
 
